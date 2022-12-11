ab6f3c0061cdbbdc511186a04cb9dbd5299fe452
Onwuchekwe David <marcusdhavid20@gmail.com>
2022-12-11 13:34:20 -0800

    combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,21 +4,19 @@
  *
  * Return: 0 (success)
  */
-
 int main(void)
-	{
+{
 	int a;
 
 	for (a = 0; a < 10; a++)
 	{
 		putchar(a + '0');
 		if (a < 9)
-	       	{
+		{
 			putchar(',');
-			putchar(' ');
+			putchar(',');
 		}
-
 	}
 	putchar('\n');
 	return (0);
-	}
+}
