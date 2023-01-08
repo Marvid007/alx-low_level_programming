8d6132be08cdcf9a3184ad1c0656033a78d0a559
Onwuchekwe David <marcusdhavid20@gmail.com>
2023-01-08 12:37:41 -0800

    4-isalpha.c

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * _islower - checks if a character is lowercase
+ * _isalpha - checks for alphabets
  * @c: the character
- * Return: 1 if letter is lowercase, 0 if not
+ *
+ * Return: 1 if character is a letter,0 if not
  */
-int _islower(int c)
+int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+
 		return (1);
 	else
 		return (0);
