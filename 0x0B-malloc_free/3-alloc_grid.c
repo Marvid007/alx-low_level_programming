bd8a69d3a043c27e9e6dbacfc6ed2b6d1e1213df
Onwuchekwe David <marcusdhavid20@gmail.com>
2023-01-09 15:09:22 -0800

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,12 +25,12 @@ int **alloc_grid(int width, int height)
 			free(p);
 			return (NULL);
 		}
-		for (i = 0, i < height, i++)
+		for (i = 0; i < height, i++)
 		{
 			p[i] = (int *) malloc(width * sizeof(int));
 			if (!p[i])
 			{
-				for (j = 0; <= i; j++)
+				for (j = 0; j <= i; j++)
 					free(p[j]);
 				free(p);
 				return (NULL);
