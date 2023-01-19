79434e4be721eaaae091922bdaccdb98328ef664
Onwuchekwe David <marcusdhavid20@gmail.com>
2023-01-19 12:09:59 -0800

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,11 +15,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(numbers, n);
 
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(numbers, int));
 		if (i < n - 1 && separator != NULL)
-			printf("%s", seperator);
+			printf("%s", separator);
 	}
 	printf("\n");
 
