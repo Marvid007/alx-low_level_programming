8b41dc465b75c65cb771caf57cf26c2e8854b43c
Onwuchekwe David <marcusdhavid20@gmail.com>
2023-01-19 10:17:02 -0800

    Task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -11,4 +11,6 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 void _putchar(char *c);
 
-#endif
+
+
+#endif /* VARIADIC_FUNCTIONS_H */
