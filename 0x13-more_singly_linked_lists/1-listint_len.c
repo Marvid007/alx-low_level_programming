24e31aa0e0e3e61885e8cea9e85eb05bf8b252c7
Onwuchekwe David <marcusdhavid20@gmail.com>
2023-02-01 08:53:06 -0800

    remaining task

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -4,19 +4,19 @@
 /**
  * listint_len Returns the number of elements
  *             in a linked listint_t list.
- * @h: A pointer to the head of the listint_t list.
+ * @h: linked list of type listint_t to traverse
  *
- * Return: The number of elements in the listint_t list.
+ * Return: The number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t num = 0;
 
 	while (h)
 	{
-		nodes++;
+		num++;
 		h = h->next;
 	}
 
-	return (nodes);
+	return (num);
 }
