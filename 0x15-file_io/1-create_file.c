e583d0f22055dc5930a21471042f1b6f4727d7e1
Onwuchekwe David <marcusdhavid20@gmail.com>
2023-02-06 15:56:29 -0800

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
  *         Otherwise - 1.
  */
 
-int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content)
 {
 	int o, w, len = 0;
 
@@ -23,6 +23,7 @@ int create_file(const char *filename, char *text_content);
 	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
